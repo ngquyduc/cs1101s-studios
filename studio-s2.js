cd507b5aef5638fa0c033bf60f63d93816e8075c
Nguyen Quy Duc <quyduc711@gmail.com>
2023-08-22 00:44:10 +0800

    init s2

---
+++ studio-s2.js
@@ -0,0 +1,55 @@
+// STUDIO S2
+
+// Q1: Write a function named biggie_size which when given a regular combo 
+// returns a biggie- sized version.
+
+
+
+
+
+// Q2: Write a function named unbiggie_size which when given a biggie-sized 
+// combo returns a non-biggie-sized version.
+
+
+
+
+
+// Q3: Write a function named is_biggie_size which when given a combo, returns 
+// true if the combo has been biggie-sized and false otherwise.
+
+
+
+
+
+// Q4: Write a function named combo_price which takes a combo and returns the 
+// price of the combo. Each patty costs $1.17, and a biggie-sized version costs 
+// $0.50 extra overall.
+
+
+
+
+
+// Q5: Write a function named empty_order which takes no arguments and returns 
+// an empty order which is represented by 0.
+
+
+
+
+
+// Q6: Write a function named add_to_order which takes an order and a combo and 
+// returns a new order which contains the contents of the old order and the new 
+// combo. For example, add_to_order(1, 2) returns 12.
+
+
+
+
+
+// Q7: Write a function named last_combo which takes an order and returns the 
+// last combo. For example, last_combo(321) returns 1.
+
+
+
+
+
+// Q8: Write a function named other_combos which takes an order and returns a 
+// new order with- out the last combo. For example, other_combos(321) returns 32.
